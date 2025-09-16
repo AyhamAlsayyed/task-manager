811a590b3eb171b9bd123375b42a9426a6583f69
Ayham Alsayyed <ayham.sayyed.04@gmail.com>
2025-09-16 03:45:17 +0300

    add the home_view that i forgot to add

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa: F403, F401
 from .auth_views import login_view, logout_view, signup_view
 from .dashboard_views import dashboard_view
+from .home_view import home_view
 from .profile_views import edit_profile_view, profile_view
 from .project_views import project_view
 from .projects_views import projects_view
